296f0f1def505403d23b7ffa261b346bdc3bb247
egamboat <egamboat@unemi.edu.ec>
2024-11-26 20:51:14 -0500

    feat: delete account

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GoogleLoginView,CustomTokenRefreshView
+from .views import GoogleLoginView,CustomTokenRefreshView,DeleteAccountView
 
 urlpatterns = [
     path("api/google-login/", GoogleLoginView.as_view(), name='google-login'),
     path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/delete-account/', DeleteAccountView.as_view(), name='delete_account'),
 ]
